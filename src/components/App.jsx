9afd9bf58b00f16a0e4f46c3a0790a7712737594
ViacheslavBFG <131298066+ViacheslavBFG@users.noreply.github.com>
2023-09-11 15:43:32 +0300

    profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import Profile from 'components/Profile/profile'
-
+import { Profile } from 'components/Profile/Profile';
+import user from '../assets/user.json';
 
 export const App = () => {
   return (
     <div className="App">
-      <Profile />
+      
+      <Profile {...user} />
     </div>
   );
 };

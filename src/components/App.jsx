c0c5ce72d314674649bb83b049200439fe338988
ViacheslavBFG <131298066+ViacheslavBFG@users.noreply.github.com>
2023-09-12 13:40:02 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Profile } from 'components/Profile/Profile';
+import { Statistics } from 'components/Statistics/Statistics';
+
 import user from '../assets/user.json';
+import data from '../assets/data.json';
 
 export const App = () => {
   return (
     <div className="App">
-      
       <Profile {...user} />
+
+      <Statistics title="Upload Stats" stats={data} />
     </div>
   );
 };
